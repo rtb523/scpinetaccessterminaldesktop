99e4bdabc05f1e84ebe209b74cc0bb2d9d0ede89
rtb523 <59374749+rtb523@users.noreply.github.com>
2021-10-26 21:16:28 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+  VC_USER : "scipnetcontact@gmail.com",
+  WEATHER_KEY : "80ade9055c570497d679aefed70a839a",
+  MAPBOX : "pk.eyJ1Ijoic2NpcG5ldCIsImEiOiJja3U4OTlocXY1c2ZnMnFwbXBxc3M4cGE5In0.XD-brByhP8gYaUWDUnlCiQ"
+}
---
+++ init.js
@@ -0,0 +1,11 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyC6YFflRMrVsQIF8-6XqyzOcWPfv3sWUkw",
+  "appId": "1:734236251278:web:9827788a7a3be965a225df",
+  "authDomain": "scipnet-terminal.firebaseapp.com",
+  "databaseURL": "https://scipnet-terminal.firebaseio.com",
+  "measurementId": "G-DFRPP3XL8M",
+  "messagingSenderId": "734236251278",
+  "projectId": "scipnet-terminal",
+  "storageBucket": "scipnet-terminal.appspot.com"
+});
